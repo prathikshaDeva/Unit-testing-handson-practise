b4afa85c533ca8fa6206b2a9061b45c0bb84261e
Prathiksha Deva Prakash <prathikshad7@gmail.com>
2022-08-03 15:32:21 +0530

    First test case'

--- test/basic-details.test.js
+++ test/basic-details.test.js
@@ -5,4 +5,8 @@ import '../src/LoanBasicDetails/BasicDetails.js';
 describe('Basic details', () => {
   // Write test cases inside this block
   // refer basic-details.js files
+  it("should access basic details", async()=>{
+    const e = await fixture(html `<basic-details></basic-details>`);
+    expect(e).to.be.accessible();
+  })
 });
